9a4f237f66365d5e8edf9e5f7e63d91d595f93cd
herasymj <jennifer.herasymuik@gmail.com>
2019-03-30 11:51:53 -0600

    Splitting data in training and testing now works

--- fraud_detection/data/split_data.py
+++ fraud_detection/data/split_data.py
@@ -1,44 +1,56 @@
+#########################################################################
+# Filename: split_data.py
+# Written By: Jennifer Herasymuik
+#
+# Purpose: the purpose of this file is to split the data set into the training
+#   data (70%) and test data (30%). It also makes sure both the test and
+#   training data sets contain some of the few fraudulent transactions
+#########################################################################
+
+
 import csv
+import random
 
 
-with open('creditcard.csv') as csvDataFile:
-    csvReader = csv.reader(csvDataFile)
-    count = 0
-    for row in csvReader:
-        if count == 0:
-            continue
-        new_transaction = Transaction(
-            time=row[0],
-            v1=row[1],
-            v2=row[2],
-            v3=row[3],
-            v4=row[4],
-            v5=row[5],
-            v6=row[6],
-            v7=row[7],
-            v8=row[8],
-            v9=row[9],
-            v10=row[10],
-            v11=row[11],
-            v12=row[12],
-            v13=row[13],
-            v14=row[14],
-            v15=row[15],
-            v16=row[16],
-            v17=row[17],
-            v18=row[18],
-            v19=row[19],
-            v20=row[20],
-            v21=row[21],
-            v22=row[22],
-            v23=row[23],
-            v24=row[24],
-            v25=row[25],
-            v26=row[26],
-            v27=row[27],
-            v28=row[28],
-            amount=row[29],
-            fraud=row[30]
-        )
-        print(str(count))
-        count += 1
+# TODO: purge data before filling csv files, so I don't have to manually do it
+with open('datasets/creditcard.csv', 'r') as csvDataFile:
+    with open('datasets/test_data.csv', 'a') as testFile:
+        with open('datasets/training_data.csv', 'a') as trainingFile:
+            csvReader = csv.reader(csvDataFile)
+            training_writer = csv.writer(trainingFile, delimiter=',', lineterminator='\n')
+            test_writer = csv.writer(testFile, delimiter=',', lineterminator='\n')
+            fraud_transactions = 492
+            total_transactions = 284807
+            training_fraud_max = fraud_transactions * 0.7   # number of fraudulent transaction we need for training
+            training_legitimate_max = (total_transactions * 0.7) - training_fraud_max   # number of legit transactions
+            test_max = total_transactions - training_legitimate_max - training_fraud_max
+            count = 0
+            train_fraud_count = 0  # Amount of current fraudulent values for training
+            training_count = 0  # Amount of current legitimate transactions for training
+            for row in csvReader:
+                if count == 0:
+                    count += 1
+                    continue
+                rand_val = random.randint(1, 10)
+                is_fraud = row[30]
+                # If the value is 7 or below, add to training data if not full
+                # TODO: Make sure to evenly distribute the fraudulent transaction
+                # TODO: Make sure not to over populate one list more than the other
+                if rand_val <= 7:
+                    if is_fraud == '1':
+                        train_fraud_count += 1
+                    training_count += 1
+                    training_writer.writerow(row)
+                    trainingFile.flush()
+                else:   # Add to testing data
+                    test_writer.writerow(row)
+                    testFile.flush()
+                print(count)
+                count += 1
+            # Document to hold statistics
+            with open('datasets/data_statistics.csv', 'a') as statsFile:
+                stats_writer = csv.writer(statsFile, lineterminator='\n')
+                stats_writer.writerow(["fraud_training", train_fraud_count])
+                stats_writer.writerow(["fraud_testing", fraud_transactions - train_fraud_count])
+                stats_writer.writerow(["legitimate_training", training_count])
+                stats_writer.writerow(["legitimate_testing", total_transactions - training_count])
